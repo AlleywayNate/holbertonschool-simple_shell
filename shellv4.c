80a80e10af219bd4511d33082a80e53ba27fb58c
AlleywayNate <4937@holbertonschool.com>
2022-07-31 18:49:36 -0700

    It kind of works

--- shellv4.c
+++ shellv4.c
@@ -9,28 +9,35 @@ int main(int argc, char **argv)
 
 void Mr_Frog(void)
 {
+	int a;
 	int test;
 	pid_t child_pid;
 	char *line;
 	size_t size = 0;
 	int status;
-	char *const args[] = {"/bin/ls", NULL};
+	char *const args[] = {"/bin/ls", "-l", NULL};
 	
   do {
 	printf("> ");
 	status = getline(&line, &size, stdin);
+	printf("%s", line);
+	for (a = 0; line[a] != '\0' ; a++)
+	{
+		if (line[a] == '\n')
+		{
+			line[a] = '\0';
+		}
+	}
+	for (a = 0; line[a]
 	child_pid = fork();
 	if (child_pid == 0)
 	{
-		execve("/bin/ls", args, NULL);
+		execve(line, args, NULL);
 	}
-	else
-	{
-		do
-		{
+	else {
+		do {
 			waitpid(child_pid, &test, WUNTRACED);
 		} while (WIFEXITED(test) == 0 && WIFSIGNALED(test) == 0);
 	}
   } while (status);
 }
-
